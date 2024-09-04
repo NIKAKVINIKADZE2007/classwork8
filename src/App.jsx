a75fd1f60ef2a81b7ff990df409e2cef76412854
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-09-04 21:08:27 +0400

    finished
    finished

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+import HomePage from './components/HomePage';
+import Layout from './components/Layout';
+import AboutUs from './components/AboutUs';
+import Projects from './components/Projects';
+import Impact from './components/Impact';
+import Locations from './components/Locations';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path='About Us' element={<AboutUs />} />
+          <Route path='Projects' element={<Projects />} />
+          <Route path='Impact' element={<Impact />} />
+          <Route path='Locations' element={<Locations />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Impact.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Impact = () => {
+  return <h1>Impact</h1>;
+};
+
+export default Impact;
