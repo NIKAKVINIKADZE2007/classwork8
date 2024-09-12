23f2bf8d56eae0af05422c638197cd1fcd362e74
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-09-12 14:44:09 +0400

    finished classwork8
    finished

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,5 +1,6 @@
-// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
 import Layout from './components/Layout';
@@ -7,12 +8,10 @@ import AboutUs from './components/AboutUs';
 import Projects from './components/Projects';
 import Impact from './components/Impact';
 import Locations from './components/Locations';
-import Rutes from './Route';
-function App() {
+const Rutes = () => {
   return (
-    <>
-      {/* pirveli xerix */}
-      {/* <Routes>
+    <div>
+      <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path='About Us' element={<AboutUs />} />
@@ -20,13 +19,9 @@ function App() {
           <Route path='Impact' element={<Impact />} />
           <Route path='Locations' element={<Locations />} />
         </Route>
-      </Routes> */}
-
-      <BrowserRouter>
-        <Rutes />
-      </BrowserRouter>
-    </>
+      </Routes>
+    </div>
   );
-}
+};
 
-export default App;
+export default Rutes;
